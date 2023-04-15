17269ed7155d15c1e7f95f81ae97fe8b1a6a9475
Tony Scinocco <scinocco.a@gmail.com>
2023-04-15 10:26:17 -0400

    quick sort practice

--- kata-machine/src/day11/QuickSort.ts
+++ kata-machine/src/day11/QuickSort.ts
@@ -1,3 +1,39 @@
-export default function quick_sort(arr: number[]): void {
+function partion(arr: number[], lo: number, hi: number): number {
+  const pivot = arr[hi];
+  let idx = lo - 1;
 
+  for (let i = lo; i < hi; i++) {
+    if (arr[i] <= pivot) {
+      idx++;
+      const tmp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = tmp;
+    }
+  }
+
+  idx++;
+
+  arr[hi] = arr[idx];
+  arr[idx] = pivot;
+
+  return idx;
+}
+
+function qs(arr: number[], lo: number, hi: number): void {
+  if (lo >= hi) {
+    // recursion ends here. we've reached the end.
+    return;
+  }
+
+  const pivotIdx = partion(arr, lo, hi);
+
+  // resort left
+  qs(arr, lo, pivotIdx - 1);
+
+  // resort right
+  qs(arr, pivotIdx + 1, hi)
+}
+
+export default function quick_sort(arr: number[]): void {
+  qs(arr, 0, arr.length - 1);
 }
